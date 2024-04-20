2d4b718853290e49d29c5c4d4c6f5861c0c0edd9
Jadeilson Martinho <jadeilsonm17@gmail.com>
2024-04-20 05:17:39 -0300

    style: fix in style

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,82 +1,124 @@
-import { FlatList, Pressable, StyleSheet } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import { Text, View } from '@/components/Themed';
-import { useEffect, useState } from 'react';
-import CardNameCity from '@/components/CardNameCity';
+import { FlatList, Pressable, StyleSheet, TextInput } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Text, View } from "@/components/Themed";
+import { useEffect, useState } from "react";
+import CardNameCity from "@/components/CardNameCity";
 
 type Uf = {
   id: number;
   sigla: string;
   nome: string;
   regiao: Regiao;
-}
+};
 
 type Mesorregiao = {
   id: number;
   nome: string;
   UF: Uf;
-}
+};
 
 type Microrregiao = {
   id: number;
   nome: string;
   mesorregiao: Mesorregiao;
-}
+};
 
 type Regiao = {
   id: number;
   sigla: string;
   nome: string;
-}
+};
 
 type City = {
   nome: string;
   microrregiao: Microrregiao;
   id: number;
-}
+};
 
 export default function TabTwoScreen() {
   const initialPage = 1;
-  const [isLoading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
   const [city, setCity] = useState([]);
-  const [pages, setPages] = useState([]);
+  const [filteredCites, setFilteredCites] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(initialPage);
-  const [itemsPerPage, setItemsPerPage] = useState([0, 10]);
-
 
   useEffect(() => {
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios')
-      .then(response => response.json())
-      .then(data => {
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/municipios")
+      .then((response) => response.json())
+      .then((data) => {
         setCity(data);
-        setPages(data.slice(itemsPerPage[0], itemsPerPage[1]));
-        setTotalPages(data.length / 10);
+        setFilteredCites(data);
+        setTotalPages(filteredCites.length / 10);
       });
-    setLoading(false);
   }, []);
 
-   return (<View style={styles.container}>
+  useEffect(() => {
+    const filtered = city.filter((item: City) =>
+      item.nome.toUpperCase().startsWith(search.toUpperCase())
+    );
+    setCurrentPage(initialPage);
+    setFilteredCites(filtered);
+    setTotalPages(filteredCites.length / 10);
+  }, [city, search]);
+
+  return (
+    <View style={styles.container}>
       <Text style={styles.title}>Municipios</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.containerInput}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'>
+        <TextInput
+          style={{ width: "90%", color: "#ffffff", fontWeight: "bold" }}
+          value={search}
+          onChangeText={setSearch}
+          placeholder='Buscar por cidade:'
+          placeholderTextColor='#fff'
+        />
+        <AntDesign name='search1' size={24} color='#ffffff' />
+      </View>
+      <View
+        style={styles.separator}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'
+      />
       <FlatList
-        data={city.slice((currentPage - 1) * 10, currentPage * 10)}
+        data={filteredCites.slice((currentPage - 1) * 10, currentPage * 10)}
         keyExtractor={(item: City, index) => index.toString()}
-        style={{width: '90%'}}
-        renderItem={({item, index}) => (
-          <CardNameCity nome={item.nome} uf={item.microrregiao.mesorregiao.UF.sigla} id={item.id} key={index} />
+        style={{ width: "90%" }}
+        renderItem={({ item, index }) => (
+          <CardNameCity
+            nome={item.nome}
+            uf={item.microrregiao.mesorregiao.UF.sigla}
+            id={item.id}
+          />
         )}
       />
-      <View style={styles.pagination} lightColor="#eee" darkColor="rgba(255,255,255,0.1)">
-        <Pressable onPress={() => setCurrentPage(currentPage <= initialPage ? initialPage : currentPage - 1)}>
-        <AntDesign name="caretleft" size={24} color="black" />
+      <View
+        style={styles.pagination}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'>
+        <Pressable
+          onPress={() =>
+            setCurrentPage(
+              currentPage > initialPage ? currentPage -1 : initialPage
+            )
+          }>
+          <AntDesign name='caretleft' size={24} color='#EE6D00' />
         </Pressable>
-        <Text style={{fontSize: 20}}>{currentPage}</Text>
-        <Pressable onPress={() => setCurrentPage(currentPage >= totalPages ? initialPage : currentPage + 1)}>
-        <AntDesign name="caretright" size={24} color="black" />
+        <Text style={{ fontSize: 18 }}>{currentPage}</Text>
+        <Pressable
+          onPress={() =>
+            setCurrentPage(
+              currentPage < totalPages ? currentPage + 1 : currentPage
+            )
+          }>
+          <AntDesign name='caretright' size={24} color='#EE6D00' />
         </Pressable>
       </View>
-    </View>)
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -94,10 +136,10 @@ const styles = StyleSheet.create({
     padding: 15,
     marginTop: 35,
     height: 55,
-    width: "80%",
-    backgroundColor: "#333333",
+    width: "90%",
+    backgroundColor: "#333",
     borderRadius: 12,
-    color: "#ffffff"
+    color: "#ffffff",
   },
   icon: {
     marginLeft: 10,
@@ -111,6 +153,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 40,
+    marginTop: 55,
     fontWeight: "bold",
     color: "#EE6D00",
   },
@@ -145,11 +188,12 @@ const styles = StyleSheet.create({
   },
   pagination: {
     flex: 0,
-    flexDirection: 'row',
-    width: '90%',
+    flexDirection: "row",
+    width: "80%",
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: '#ce5050',
-  }  
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#000000",
+    marginBottom: 40
+  },
 });
